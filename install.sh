30826a99ae2f95a3efdea451f096490dfbfb17ce
norb_ <127006485+devnorb@users.noreply.github.com>
2023-10-03 16:29:39 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ main() {
     echo -e "Install script by norb_ and vexus_dev"
     echo -e "Downloading current release..."
     curl "https://raw.githubusercontent.com/devnorb/log/main/public-beta.js" -o "./index.js"
+    echo -e "Installing dependencies"
     npm i axios crawler-detect discord.js express static-vpn-check
+    echo -e "Finished Installation. [ Follow step 3 in discord for more information. ]"
+    echo -e "norb_ and vexus_dev on top"
 }
 
 main
